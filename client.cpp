d938913b9d1e00335f7181066d6c477605423845
Andrew S. Grigorev <andrew@ei-grad.ru>
2010-04-02 01:38:10 +0600

    Initial commit

---
+++ client.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2009 Andrew S. Grigorev, Chelyabinsk State University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <cstdlib>
+#include <cstring>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <gmpxx.h>
+
+#include "tossing.h"
+#include "config.h"
+
+
+int Connect(char * address){
+    int sock;
+    struct sockaddr_in server;
+
+    /* Create the TCP socket */
+    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        fprintf(stderr, "Failed to create socket\n");
+        exit(1);
+    }
+    
+    /* Construct the server sockaddr_in structure */
+    memset(&server, 0, sizeof(server));       /* Clear struct */
+    server.sin_family = AF_INET;                  /* Internet/IP */
+    server.sin_addr.s_addr = inet_addr(address);  /* IP address */
+    server.sin_port = htons(PORT);                /* server port */
+    
+    /* Establish connection */
+    if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        fprintf(stderr, "Failed to connect\n");
+        exit(1);
+    }
+
+    return sock;
+}
+
+int read_rsa(mpz_class &private_key) {
+    
+    return 0;
+}
+
+int main(int argc, char * argv[]){
+
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: client <server_ip>\n");
+        exit(1);
+    }
+
+    int sock = Connect(argv[2]);
+
+    mpz_class private_key;
+    read_rsa(private_key);
+
+    return 0;
+}
+
