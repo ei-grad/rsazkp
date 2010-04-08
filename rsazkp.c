2a9f2afe7a642f3ca7373352ae79ba41cc19a2e7
Andrew S. Grigorev <andrew@ei-grad.ru>
2010-04-08 14:14:41 +0600

    clientAct1 finished

--- rsazkp.c
+++ rsazkp.c
@@ -23,18 +23,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "openssl/bio.h"
-#include "openssl/ssl.h"
-#include "openssl/err.h"
-#include "openssl/rsa.h"
-#include "openssl/bn.h"
+#include <openssl/ssl.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/rsa.h>
+#include <openssl/bn.h>
+#include <openssl/ui.h>
 
+static UI_METHOD *ui_method = NULL;
+static BIO * bio_out;
+static BIO * bio_err;
 
-BIO * out;
-BIO * err;
+#define PW_MIN_LENGTH 4
+typedef struct pw_cb_data
+{
+	const void *password;
+	const char *prompt_info;
+} PW_CB_DATA;
 
+void destroy_ui_method(void) {
+	if(ui_method) {
+		UI_destroy_method(ui_method);
+		ui_method = NULL;
+	}
+}
 
-int ExtendedEuclidianAlgorithm(BIGNUM * a, BIGNUM * b, BIGNUM * d, BIGNUM * x, BIGNUM * y){
+int password_callback(char *buf, int bufsiz, int verify, PW_CB_DATA *cb_tmp) {
+	UI *ui = NULL;
+	int res = 0;
+	const char *prompt_info = NULL;
+	const char *password = NULL;
+	PW_CB_DATA *cb_data = (PW_CB_DATA *)cb_tmp;
+
+	if (cb_data) {
+		if (cb_data->password)
+			password = cb_data->password;
+		if (cb_data->prompt_info)
+			prompt_info = cb_data->prompt_info;
+	}
+
+	if (password) {
+		res = strlen(password);
+		if (res > bufsiz)
+			res = bufsiz;
+		memcpy(buf, password, res);
+		return res;
+	}
+
+	ui = UI_new_method(ui_method);
+	if (ui) {
+		int ok = 0;
+		char *buff = NULL;
+		int ui_flags = 0;
+		char *prompt = NULL;
+
+		prompt = UI_construct_prompt(ui, "pass phrase",
+			prompt_info);
+
+		ui_flags |= UI_INPUT_FLAG_DEFAULT_PWD;
+		UI_ctrl(ui, UI_CTRL_PRINT_ERRORS, 1, 0, 0);
+
+		if (ok >= 0)
+			ok = UI_add_input_string(ui,prompt,ui_flags,buf,
+				PW_MIN_LENGTH,BUFSIZ-1);
+		if (ok >= 0 && verify)
+			{
+			buff = (char *)OPENSSL_malloc(bufsiz);
+			ok = UI_add_verify_string(ui,prompt,ui_flags,buff,
+				PW_MIN_LENGTH,BUFSIZ-1, buf);
+			}
+		if (ok >= 0)
+			do {
+				ok = UI_process(ui);
+			} while (ok < 0 && UI_ctrl(ui, UI_CTRL_IS_REDOABLE, 0, 0, 0));
+
+		if (buff) {
+			OPENSSL_cleanse(buff,(unsigned int)bufsiz);
+			OPENSSL_free(buff);
+		}
+
+		if (ok >= 0)
+			res = strlen(buf);
+		if (ok == -1) {
+			BIO_printf(bio_err, "User interface error\n");
+			ERR_print_errors(bio_err);
+			OPENSSL_cleanse(buf,(unsigned int)bufsiz);
+			res = 0;
+		}
+		if (ok == -2) {
+			BIO_printf(bio_err,"aborted!\n");
+			OPENSSL_cleanse(buf,(unsigned int)bufsiz);
+			res = 0;
+		}
+		UI_free(ui);
+		OPENSSL_free(prompt);
+	}
+	return res;
+}
+
+int ExtendedEuclidianAlgorithm(BIGNUM * a, BIGNUM * b, BIGNUM * d, BIGNUM * x, BIGNUM * y) {
     
     if (BN_is_zero(b)) {
         BN_copy(d, a);
@@ -57,7 +144,7 @@ int ExtendedEuclidianAlgorithm(BIGNUM * a, BIGNUM * b, BIGNUM * d, BIGNUM * x, B
     BIGNUM * q = BN_new();
     BIGNUM * r = BN_new();
 
-    BIGNUM * tmp1 = BN_new();
+    BIGNUM * tmp = BN_new();
 
     BN_copy(a1, a);
     BN_copy(b1, b);
@@ -67,26 +154,26 @@ int ExtendedEuclidianAlgorithm(BIGNUM * a, BIGNUM * b, BIGNUM * d, BIGNUM * x, B
     BN_zero(y2);
     BN_one(y1);
 
-    while (!BN_is_zero(b)) {
+    while (!BN_is_zero(b1)) {
         
         // I
 
-        BN_div(q, NULL, a, b, ctx); // q = floor(a / b)
+        BN_div(q, NULL, a1, b1, ctx); // q = floor(a1 / b1)
 
-        BN_mul(tmp1, q, b, ctx);
-        BN_sub(r, a, tmp);          // r = a - q * b
+        BN_mul(tmp, q, b1, ctx);
+        BN_sub(r, a1, tmp);          // r = a1 - q * b1
         
-        BN_mul(tmp1, q, x1, ctx);
-        BN_sub(x, x2, tmp1);        // x = x2 - q * x1
+        BN_mul(tmp, q, x1, ctx);
+        BN_sub(x, x2, tmp);        // x = x2 - q * x1
 
-        BN_mul(tmp1, q, y1, ctx);
-        BN_sub(y, y2, tmp1);        // y = y2 - q * y1
+        BN_mul(tmp, q, y1, ctx);
+        BN_sub(y, y2, tmp);        // y = y2 - q * y1
 
 
         // II
         
-        BN_copy(a, b);              // a <- b
-        BN_copy(b, r);              // b <- r
+        BN_copy(a1, b1);              // a1 <- b1
+        BN_copy(b1, r);              // b1 <- r
         BN_copy(x2, x1);            // x2 <- x1
         BN_copy(x1, x);             // x1 <- x
         BN_copy(y2, y1);            // y2 <- y1
@@ -96,7 +183,7 @@ int ExtendedEuclidianAlgorithm(BIGNUM * a, BIGNUM * b, BIGNUM * d, BIGNUM * x, B
 
     // Result
     
-    BN_copy(d, a);
+    BN_copy(d, a1);
     BN_copy(x, x2);
     BN_copy(y, y2);
 
@@ -116,7 +203,7 @@ int ExtendedEuclidianAlgorithm(BIGNUM * a, BIGNUM * b, BIGNUM * d, BIGNUM * x, B
     BN_free(q);
     BN_free(r);
 
-    BN_free(tmp1);
+    BN_free(tmp);
 
     return 0;
 }
@@ -145,39 +232,46 @@ int GetMultiplicativeInverse(BIGNUM * res, BIGNUM * a, BIGNUM * n) {
     return ret;
 }
 
-struct Client {
+typedef struct client {
     RSA * rsa;
     BIGNUM * k;
     BIGNUM * m;
     BIO * conn;
-};
+} Client;
 
-
-
-int Act1(Client &client) {
+int clientAct1(Client * client) {
+    
+    char * tmpstr;
 
     BIGNUM * k = BN_new();
     BIGNUM * m = BN_new();
     
-    BIO_write(bio_out, "Этап 1\n");
+    BIO_puts(bio_out, "Этап 1\n");
 
     do {
-        BN_rand(k, (rsa->e.top - 1) * sizeof(BN_ULONG), -1, false);
-        BIO_write(out, "Ищем обратный по умножению к числу ");
-        BN_print(out, k);
-        BIO_write(out, "\n");
-    } while(GetMultiplicativeInverse(m, k, rsa->n));
+        BN_rand_range(k, client->rsa->n);
+        tmpstr = BN_bn2dec(k);
+        printf("Ищем обратный по умножению к числу %s\n", tmpstr);
+        OPENSSL_free(tmpstr);
+    } while(GetMultiplicativeInverse(m, k, client->rsa->n));
 
-    BIO_write("k = ");
-    BN_print(out, k);
-    BIO_write("\nm = ");
-    BN_print(out, m);
-    BIO_write(out, "\n");
+    client->k = k;
+    client->m = m;
+    
+    BIO_puts(bio_out, "Отправляем k и m серверу...\n");
 
-    BIO_write(out, "Отправляем k и m серверу...");
-    BN_print(client.conn, k);
-    BN_print(client.conn, m);
+    tmpstr = BN_bn2dec(k);
+    printf("k = %s\n", tmpstr);
+    BIO_write(client->conn, tmpstr, strlen(tmpstr));
+    BIO_write(client->conn, "\n", 1);
+    OPENSSL_free(tmpstr);
 
+    tmpstr = BN_bn2dec(m);
+    printf("m = %s\n", tmpstr);
+    BIO_write(client->conn, tmpstr, strlen(tmpstr));
+    BIO_write(client->conn, "\n", 1);
+    OPENSSL_free(tmpstr);
+    
     return 0;
 }
 
@@ -192,10 +286,9 @@ int HandleClient(BIO * bio_client){
     return 0;
 }
 
-int server(char ** argv) {
+int serverMain(char ** argv) {
     int i;
     BIO * bio_listen = BIO_new_accept(*argv);
-    argv++;
     BIO_set_bind_mode(bio_listen, BIO_BIND_REUSEADDR);
     /* First call to BIO_accept() sets up accept BIO */
     if(BIO_do_accept(bio_listen) <= 0) {
@@ -209,7 +302,6 @@ int server(char ** argv) {
             return 1;
         }
         fprintf(stderr, "Connection established!\n");
-        BIO * bio_client;
         HandleClient(BIO_pop(bio_listen));
     }
     BIO_free(bio_listen);
@@ -219,12 +311,11 @@ int server(char ** argv) {
 int clientConnect(Client * client, char * addr) {
     char tmpstr[1024];
     int bytes;
-    Client client;
 
-    client->conn = BIO_new_connect(*argv);
+    client->conn = BIO_new_connect(addr);
 
-    if (BIO_do_connect(Client->conn) <= 0) {
-        BIO_write(err, "Error connecting to server!\n");
+    if (BIO_do_connect(client->conn) <= 0) {
+        BIO_puts(bio_err, "Error connecting to server!\n");
         return 1;
     }
     return 0;
@@ -237,16 +328,16 @@ int clientReadKey(Client * client, char * key) {
     }
     
     client->rsa = RSA_new();
-    if (client->rsa == NULL) {
+    if (client->rsa == NULL)
         return 1;
-    }
 
-    PEM_read_RSAPrivateKey(f, &client->rsa, password_cb, NULL);
+    if(PEM_read_RSAPrivateKey(f, &client->rsa, password_callback, NULL) == 0)
+        return 1;
 
     return 0;
 }
 
-int clientFree(client) {
+int clientFree(Client client) {
     BIO_free(client.conn);
     RSA_free(client.rsa);
     BN_free(client.k);
@@ -259,14 +350,16 @@ int clientMain(char ** argv) {
     Client client;
 
     if(clientConnect(&client, argv[0])) {
+        BIO_puts(bio_err, "Не удалось установить соединение с сервером!\n");
         return 1;
     }
 
     if(clientReadKey(&client, argv[1])) {
+        BIO_puts(bio_err, "Не удалось считать ключ!\n");
         return 1;
     }
 
-    Act1(client);
+    clientAct1(&client);
     
     clientFree(client);
     return 0;
@@ -278,14 +371,15 @@ int main(int argc, char * argv[]) {
 
     int ret=1;
 
-    out = BIO_new_fp(stdout, BIO_NOCLOSE);
-    err = BIO_new_fp(stderr, BIO_NOCLOSE);
+    bio_out = BIO_new_fp(stdout, BIO_NOCLOSE);
+    bio_err = BIO_new_fp(stderr, BIO_NOCLOSE);
 
     SSL_load_error_strings();
     SSL_library_init();
     
     if (argc != 4) {
         fprintf(stderr, "%s\n", using);
+        return 1;
     }
     argc--; argv++;
     if (strcmp("client", *argv) == 0) {
