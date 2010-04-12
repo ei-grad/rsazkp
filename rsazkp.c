0d84cd53b0091a7a94741582166e329c2551ff6e
Andrew S. Grigorev <andrew@ei-grad.ru>
2010-04-12 17:08:52 +0600

    Release 1.0

--- rsazkp.c
+++ rsazkp.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2009 Andrew S. Grigorev, Chelyabinsk State University
+ * Copyright (c) 2009-2010 Andrew S. Grigorev <andrew@ei-grad.ru>
+ * Chelyabinsk State University
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,11 +25,40 @@
 #include <stdlib.h>
 
 #include <openssl/ssl.h>
+#include <openssl/rand.h>
 #include <openssl/bio.h>
 #include <openssl/err.h>
 #include <openssl/rsa.h>
 #include <openssl/bn.h>
 #include <openssl/ui.h>
+
+
+typedef struct server {
+    BIO * conn;
+    RSA * rsa;
+    // Act 1
+    BIGNUM * k;
+    BIGNUM * m;
+    // Act 2
+    BIGNUM * C;
+    // Act 3
+    BIGNUM * X;
+} Server;
+
+
+typedef struct client {
+    BIO * conn;
+    RSA * rsa;
+    // Act 1
+    BIGNUM * k;
+    BIGNUM * m;
+    // Act2
+    BIGNUM * C;
+    // Act3
+    BIGNUM * M;
+    BIGNUM * X;
+} Client;
+
 
 static UI_METHOD *ui_method = NULL;
 static BIO * bio_out;
@@ -222,6 +252,11 @@ int GetMultiplicativeInverse(BIGNUM * res, BIGNUM * a, BIGNUM * n) {
 
     if (BN_is_one(d)) {
         BN_copy(res, x);
+        
+        if (res->neg) {
+            BN_add(res, res, n);
+        }
+
         ret = 0;
     }
 
@@ -232,28 +267,44 @@ int GetMultiplicativeInverse(BIGNUM * res, BIGNUM * a, BIGNUM * n) {
     return ret;
 }
 
-typedef struct client {
-    RSA * rsa;
-    BIGNUM * k;
-    BIGNUM * m;
-    BIO * conn;
-} Client;
 
 int clientAct1(Client * client) {
     
+    int bytes;
+    char buf[1024];
     char * tmpstr;
+
+    printf("\n==== Этап 1 ====\n\n");
 
     BIGNUM * k = BN_new();
     BIGNUM * m = BN_new();
-    
-    BIO_puts(bio_out, "Этап 1\n");
 
+    BIGNUM * phy = BN_new();
+    BIGNUM * tmp1 = BN_new();
+    BIGNUM * tmp2 = BN_new();
+    BIGNUM * bn_one = BN_new();
+
+    BN_one(bn_one);
+
+    BN_sub(tmp1, client->rsa->p, bn_one);
+    BN_sub(tmp2, client->rsa->q, bn_one);
+
+    BN_CTX * ctx = BN_CTX_new();
+    BN_mul(phy, tmp1, tmp2, ctx);
+
+    tmpstr = BN_bn2dec(phy);
+    printf("phy(n) = %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+    
     do {
-        BN_rand_range(k, client->rsa->n);
+        BN_rand_range(k, phy);
         tmpstr = BN_bn2dec(k);
         printf("Ищем обратный по умножению к числу %s\n", tmpstr);
         OPENSSL_free(tmpstr);
-    } while(GetMultiplicativeInverse(m, k, client->rsa->n));
+    } while(GetMultiplicativeInverse(m, k, phy));
+
+    BN_mod_mul(m, m, client->rsa->e, phy, ctx);
+    BN_CTX_free(ctx);
 
     client->k = k;
     client->m = m;
@@ -262,47 +313,344 @@ int clientAct1(Client * client) {
 
     tmpstr = BN_bn2dec(k);
     printf("k = %s\n", tmpstr);
-    BIO_write(client->conn, tmpstr, strlen(tmpstr));
-    BIO_write(client->conn, "\n", 1);
+    BIO_write(client->conn, tmpstr, strlen(tmpstr)+1);
     OPENSSL_free(tmpstr);
+
+    bytes = BIO_read(client->conn, buf, 1024);
+    buf[bytes] = 0;
+    if (strcmp(buf, "OK") != 0)
+        return 1;
 
     tmpstr = BN_bn2dec(m);
     printf("m = %s\n", tmpstr);
-    BIO_write(client->conn, tmpstr, strlen(tmpstr));
-    BIO_write(client->conn, "\n", 1);
+    BIO_write(client->conn, tmpstr, strlen(tmpstr)+1);
     OPENSSL_free(tmpstr);
+    
+
+    bytes = BIO_read(client->conn, buf, 1024);
+    buf[bytes] = 0;
+    if (strcmp(buf, "OK") != 0)
+        return 1;
+
     
     return 0;
 }
 
-int HandleClient(BIO * bio_client){
+int serverAct1(Server * server) {
     int bytes;
-    char tmpstr[1024];
-    bytes = BIO_read(bio_client, tmpstr, 1024);
-    tmpstr[bytes] = 0;
-    printf("%s\n", tmpstr);
-    BIO_puts(bio_client, "Hello, client!");
-    BIO_free(bio_client);
+    char buf[1024];
+    char * bufptr = buf;
+    char * tmpstr;
+
+    printf("\n==== Этап 1 ====\n\n");
+    
+    server->k = BN_new();
+    server->m = BN_new();
+
+    bytes = BIO_read(server->conn, buf, 1024);
+    buf[bytes] = 0;
+    BN_dec2bn(&server->k, buf);
+    tmpstr = BN_bn2dec(server->k);
+    printf("k = %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+
+    BIO_puts(server->conn, "OK");
+
+    bytes = BIO_read(server->conn, buf, 1024);
+    buf[bytes] = 0;
+    BN_dec2bn(&server->m, buf);
+    tmpstr = BN_bn2dec(server->m);
+    printf("m = %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+
+    BIO_puts(server->conn, "OK");
+
+    return 0;
+}
+
+int clientAct2(Client * client) {
+
+    int bytes;
+    char * tmpstr;
+    char buf[1024];
+
+    printf("\n==== Этап 2 ====\n\n");
+
+    client->C = BN_new();
+    BN_rand_range(client->C, client->rsa->n);
+
+    tmpstr = BN_bn2dec(client->C);
+    printf("Шифртекст: %s\n", tmpstr);
+    BIO_puts(client->conn, tmpstr);
+    OPENSSL_free(tmpstr);
+    
+    bytes = BIO_read(client->conn, buf, 1024);
+    buf[bytes] = 0;
+    if (strcmp(buf, "OK") != 0)
+        return 1;
+   client->rsa->n,  
+    printf("Шифртест передан.\n");
+
+    return 0;
+}
+
+int serverAct2(Server * server) {
+
+    int bytes;
+    char buf[1024];
+    
+    printf("\n==== Этап 2 ====\n\n");
+
+    bytes = BIO_read(server->conn, buf, 1024);
+    buf[bytes] = 0;
+
+    printf("Получен шифртекст: %s\n", buf);
+
+    server->C = BN_new();
+
+    if (!BN_dec2bn(&server->C, buf)) {
+        fprintf(stderr, "Ошибка при получении шифртекста!");
+        BIO_puts(server->conn, "ERR");
+        return 1;
+    }
+    
+    BIO_puts(server->conn, "OK");
+    printf("Шифртест принят.\n");
+    
+    return 0;
+}
+
+int clientAct3(Client * client) {
+
+    int bytes;
+    char buf[1024];
+    char * tmpstr;
+
+    printf("\n==== Этап 3 ====\n\n");
+
+    client->M = BN_new();
+    client->X = BN_new();
+
+    BN_CTX * ctx = BN_CTX_new();
+
+    BN_mod_exp(client->M, client->C, client->rsa->d, client->rsa->n, ctx);
+    
+    tmpstr = BN_bn2dec(client->M);
+    printf("M = %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+
+    BN_mod_exp(client->X, client->M, client->k, client->rsa->n, ctx);
+
+    tmpstr = BN_bn2dec(client->X);
+    BIO_puts(client->conn, tmpstr);
+    printf("X = %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+
+    BIGNUM * x = BN_new();
+    BN_mod_exp(x, client->C, client->rsa->d, client->rsa->n, ctx);
+    BN_mod_exp(x, x, client->rsa->e, client->rsa->n, ctx);
+    tmpstr = BN_bn2dec(x);
+    printf("x = %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+    
+    bytes = BIO_read(client->conn, buf, 1024);
+    buf[bytes] = 0;
+    if (strcmp(buf, "OK") != 0) {
+        printf("%s\n", buf);
+        return 1;
+    }
+    
+    printf("X передан.\n");
+    
+    BN_CTX_free(ctx);
+    return 0;
+}
+
+int serverAct3(Server * server) {
+
+    int bytes;
+    char buf[1024];
+    
+    printf("\n==== Этап 3 ====\n\n");
+
+    bytes = BIO_read(server->conn, buf, 1024);
+    buf[bytes] = 0;
+
+    printf("Получен X: %s\n", buf);
+
+    server->X = BN_new();
+
+    if (!BN_dec2bn(&server->X, buf)) {
+        fprintf(stderr, "Ошибка при получении X!");
+        BIO_puts(server->conn, "ERR");
+        return 1;
+    }
+    
+    BIO_puts(server->conn, "OK");
+    printf("X принят.\n");
+    
+    return 0;
+}
+
+int clientAct4(Client * client) {
+
+    int bytes;
+    char buf[1024];
+
+    printf("\n==== Этап 4 ====\n\n");
+
+    BIO_puts(client->conn, "How do you do?");
+
+    bytes = BIO_read(client->conn, buf, 1024);
+    buf[bytes] = 0;
+    if (strcmp(buf, "OK") != 0) {
+        printf("Сервер отверг доказательство!\n");
+        return 1;
+    }
+    
+    printf("Сервер принял доказательство.\n");
+    return 0;
+}
+
+int serverAct4(Server * server) {
+
+    int bytes;
+    char buf[1024];
+    char * tmpstr;
+    int ret = 1;
+    
+    printf("\n==== Этап 4 ====\n\n");
+    
+    bytes = BIO_read(server->conn, buf, 1024);
+    buf[bytes] = 0;
+    if (strcmp(buf, "How do you do?") != 0)
+        return 1;
+    
+    BIGNUM * x = BN_new();
+    BN_CTX * ctx = BN_CTX_new();
+
+    BN_mod_exp(x, server->X, server->m, server->rsa->n, ctx);
+
+    tmpstr = BN_bn2dec(x);
+    printf("x = %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+
+    if ( BN_cmp(x, server->C) == 0 ) {
+        BIO_puts(server->conn, "OK");
+        printf("Проверка пройдена. Клиенту отправлено подтверждение.\n");
+        ret = 0;
+    } else {
+        BIO_puts(server->conn, "ERR");
+        printf("Тест провален. Клиент оповещен о неудаче.\n");
+    }
+    
+    BN_free(x);
+    BN_CTX_free(ctx);
+    
+    return ret;
+}
+
+int clientMain(char ** argv) {
+    
+    Client client;
+
+    if(clientConnect(&client, argv[0])) {
+        BIO_puts(bio_err, "Не удалось установить соединение с сервером!\n");
+        return 1;
+    }
+
+    if(clientReadKey(&client, argv[1])) {
+        BIO_puts(bio_err, "Не удалось считать ключ!\n");
+        return 1;
+    }
+
+    if(clientAct1(&client)) {
+        printf("Ошибка при прохождении первого этапа!\n");
+        return 1;
+    }
+
+    if(clientAct2(&client)) {
+        printf("Ошибка при прохождении второго этапа!\n");
+        return 1;
+    }
+    
+    if(clientAct3(&client)) {
+        printf("Ошибка при прохождении третьего этапа!\n");
+        return 1;
+    }
+
+    if(clientAct4(&client)) {
+        printf("Ошибка при прохождении четвертого этапа!\n");
+        return 1;
+    }
+
+    printf("Доказательство завершено!\n");
+
+    clientFree(&client);
+    return 0;
+}
+
+int serverHandleClient(Server * server){
+    
+    if(serverAct1(server)) {
+        printf("Ошибка при прохождении первого этапа!\n");
+        return 1;
+    }
+    if(serverAct2(server)) {
+        printf("Ошибка при прохождении второго этапа!\n");
+        return 1;
+    }
+    if(serverAct3(server)) {
+        printf("Ошибка при прохождении третьего этапа!\n");
+        return 1;
+    }
+
+    if(serverAct4(server)) {
+        printf("Ошибка при прохождении четвертого этапа!\n");
+        return 1;
+    }
+
+    printf("Проверка завершена!\n\n");
+
     return 0;
 }
 
 int serverMain(char ** argv) {
+
+    Server server;
     int i;
-    BIO * bio_listen = BIO_new_accept(*argv);
+    
+    if (serverReadKey(&server, argv[1])) {
+        fprintf(stderr, "Can't read key from file %s!", argv[1]);
+        return 1;
+    }
+
+    BIO * bio_listen = BIO_new_accept(argv[0]);
     BIO_set_bind_mode(bio_listen, BIO_BIND_REUSEADDR);
+
     /* First call to BIO_accept() sets up accept BIO */
     if(BIO_do_accept(bio_listen) <= 0) {
         fprintf(stderr, "Error setting up accept\n");
         return 1;
     }
-    for(i=0;i<10;i++){
+
+    for(i=0; i < 10; i++){
+
         /* Wait for incoming connection */
         if(BIO_do_accept(bio_listen) <= 0) {
-            fprintf(stderr, "Error accepting connection\n");
+            fprintf(stderr, "Ошибка подключения!\n");
             return 1;
         }
-        fprintf(stderr, "Connection established!\n");
-        HandleClient(BIO_pop(bio_listen));
+        fprintf(stderr, "Соединение установлено.\n");
+
+        server.conn = BIO_pop(bio_listen);
+
+        if(serverHandleClient(&server)) {
+            printf("Клиент не смог доказать знание закрытого ключа RSA!\n\n\n");
+        } else {
+            printf("Клиент доказал знание закрытого ключа RSA.\n\n\n");
+        }
+        BIO_free(server.conn);
     }
     BIO_free(bio_listen);
     return 0;
@@ -331,37 +679,90 @@ int clientReadKey(Client * client, char * key) {
     if (client->rsa == NULL)
         return 1;
 
-    if(PEM_read_RSAPrivateKey(f, &client->rsa, password_callback, NULL) == 0)
+    if(PEM_read_RSAPrivateKey(f, &client->rsa, (pem_password_cb *) password_callback, NULL) == 0)
         return 1;
+
+    char * tmpstr;
+
+    tmpstr = BN_bn2dec(client->rsa->n);
+    printf("Модуль: %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+
+    tmpstr = BN_bn2dec(client->rsa->e);
+    printf("Публичная экспонента: %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+    
+    tmpstr = BN_bn2dec(client->rsa->d);
+    printf("Секретная экспонента: %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+
+    printf("&p = %x\n", client->rsa->p);
+    if (client->rsa->p){
+        tmpstr = BN_bn2dec(client->rsa->p);
+        printf("p = %s\n", tmpstr);
+        OPENSSL_free(tmpstr);
+    }
+    
+    printf("&q = %x\n", client->rsa->q);
+    if (client->rsa->q){
+        tmpstr = BN_bn2dec(client->rsa->q);
+        printf("q = %s\n", tmpstr);
+        OPENSSL_free(tmpstr);
+    }
 
     return 0;
 }
 
-int clientFree(Client client) {
-    BIO_free(client.conn);
-    RSA_free(client.rsa);
-    BN_free(client.k);
-    BN_free(client.m);
+int serverReadKey(Server * server, char * key) {
+
+    FILE * f;
+    
+    if ((f = fopen(key, "r")) == NULL) {
+        fprintf(stderr, "Can't open file %s!\n", key);
+        return 1;
+    }
+    
+    server->rsa = RSA_new();
+    if (server->rsa == NULL)
+        return 1;
+
+    if(PEM_read_RSAPublicKey(f, &server->rsa, (pem_password_cb *) password_callback, NULL) == 0) {
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+    char * tmpstr;
+
+    tmpstr = BN_bn2dec(server->rsa->n);
+    printf("Модуль: %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+
+    tmpstr = BN_bn2dec(server->rsa->e);
+    printf("Публичная экспонента: %s\n", tmpstr);
+    OPENSSL_free(tmpstr);
+    
+    printf("&p = %x\n", server->rsa->p);
+    if (server->rsa->p){
+        tmpstr = BN_bn2dec(server->rsa->p);
+        printf("p = %s\n", tmpstr);
+        OPENSSL_free(tmpstr);
+    }
+
     return 0;
 }
 
-int clientMain(char ** argv) {
-    
-    Client client;
+int serverFree(Server * server) {
+    BIO_free(server->conn);
+    RSA_free(server->rsa);
+    BN_free(server->k);
+    BN_free(server->m);
+}
 
-    if(clientConnect(&client, argv[0])) {
-        BIO_puts(bio_err, "Не удалось установить соединение с сервером!\n");
-        return 1;
-    }
-
-    if(clientReadKey(&client, argv[1])) {
-        BIO_puts(bio_err, "Не удалось считать ключ!\n");
-        return 1;
-    }
-
-    clientAct1(&client);
-    
-    clientFree(client);
+int clientFree(Client * client) {
+    BIO_free(client->conn);
+    RSA_free(client->rsa);
+    BN_free(client->k);
+    BN_free(client->m);
     return 0;
 }
 
